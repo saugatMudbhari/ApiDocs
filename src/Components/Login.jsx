164746dedb5edc7010be4452d4611f32ab6c4136
saugat <smudbhari85@gmail.com>
2023-11-22 16:46:45 +0545

    a part where the api link is differentiate from other key:value pair

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,25 +2,48 @@ import React from 'react'
 import { Card, Container, Header } from 'semantic-ui-react'
 import { contents } from './Content'
 const Login = () => {
+    const mapComponents = () => {
+        return contents.map((content, index) => (
+            <Card key={index} style={{ width: '100%', padding: '0 2rem 2rem 2rem' }}>
+                <Header className='headerSection' style={{ marginTop: '1.3em' }} as='h2'>
+                    {content.Title}
+
+                </Header>
+
+                {Object.keys(content).map((keyName) => (
+                    keyName == 'API' ?
+                        (<Card.Header as='h4' key={keyName}>
+                            {keyName}: <span style={{ color: 'red', background: '#fbffb8', padding: '5px' }}>
+                                {content[keyName]}
+                            </span>
+                        </Card.Header>
+                        ) : (
+                            <Card.Header as='h4' key={keyName}>
+                                {keyName}:{content[keyName]}
+                            </Card.Header>
+                        )
+                ))}
+            </Card>
+        ));
+    }
     return (
         <>
             <Container>
-                {contents.map((content, index) => (
+                {/* {contents.map((content, index) => (
                     <Card key={index} style={{ width: '100%', padding: '0 2rem 2rem 2rem' }}>
                         <Header className='headerSection' style={{ marginTop: '1.3em' }} as='h2'>{content.Title}</Header>
                         {
                             Object.keys(content).map((keyName) => (
-                                <Card.Header as='h5' key={keyName}>
+                                < Card.Header as='h5' key={keyName} >
                                     {keyName}:{content[keyName]}
                                 </Card.Header>
                             ))
                         }
                     </Card>
-
-                ))}
-            </Container>
+                ))} */}
+                {mapComponents()}
+            </Container >
         </>
     )
 }
-
 export default Login
