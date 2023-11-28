6a74d2bbf099998fb64fcc6081bb382630088d82
saugat <smudbhari85@gmail.com>
2023-11-28 13:32:58 +0545

    clickevent added

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import { Card, Container, Grid, Segment, Header } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Container, Grid } from 'semantic-ui-react'
 import { contents } from './Content'
-const Login = () => {
 
+const Login = () => {
+    const [isClick, setIsClick] = useState(false);
+    const handleClick = (event) => {
+        setIsClick(current => !current);
+    };
+    { console.log(isClick) }
     const mapComponents = () => {
         return contents.map((content, index) => (
-
             <div style={{ display: 'flex', width: '100%', border: '1px solid red', marginBottom: '2rem', }}>
                 <div style={{ width: '15%', }}>
                     {Object.keys(content).map((keyName) => (
@@ -20,15 +24,28 @@ const Login = () => {
                         }
                         else if (keyName == 'languages') {
                             return (
-                                <Grid columns={5} >
-                                    <Grid.Row style={{ margin: 0, padding: 10 }} >
-                                        {content[keyName].map((language, index) => (
-                                            <Grid.Column  >
-                                                {language.language}
-                                            </Grid.Column>
-                                        ))}
-                                    </Grid.Row>
-                                </Grid>
+                                <div>
+                                    <Grid columns={5} >
+                                        <Grid.Row style={{ marginTop: 4, padding: 0 }} >
+                                            {content[keyName].map((language, index) => (
+                                                <Grid.Column onClick={handleClick}>
+                                                    <h5 style={{ padding: 10 }} className='language_list'> {language.language}</h5>
+                                                </Grid.Column>
+                                            ))}
+                                        </Grid.Row>
+                                    </Grid>
+                                    <Grid columns={1} >
+                                        <Grid.Row style={{ margin: 0, padding: 10 }} >
+                                            {content[keyName].map((language, index) => (
+                                                <Grid.Column   >
+                                                    {isClick && (<code style={{}}>
+                                                        {language.code}
+                                                    </code>)}
+                                                </Grid.Column>
+                                            ))}
+                                        </Grid.Row>
+                                    </Grid>
+                                </div>
                             );
                         }
                         else {
@@ -38,10 +55,7 @@ const Login = () => {
                     })}
                 </div>
             </div >
-
         ));
-
-
     }
 
     return (
