c3709ade752e4aacf2809bcdfaafa6b5d5c9f184
saugat <smudbhari85@gmail.com>
2023-11-29 01:24:32 +0545

    copy-to-clipboard

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,73 +1,116 @@
-import React, { useState } from 'react'
-import { Container, Grid } from 'semantic-ui-react'
-import { contents } from './Content'
+import React, { useState } from "react";
+import { Container, Grid, Icon } from "semantic-ui-react";
+import { contents } from "./Content";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const Login = () => {
-    const [isClick, setIsClick] = useState(false);
     const [clickedLanguage, setClickedLanguage] = useState(null);
+    const [copySuccess, setCopySuccess] = useState(false);
     const handleClick = (language) => {
-        setIsClick(current => !current);
         setClickedLanguage(language);
+
     };
-    { console.log(isClick) }
+
     const mapComponents = () => {
         return contents.map((content, index) => (
-            <div style={{ display: 'flex', width: '100%', border: '1px solid red', marginBottom: '2rem', }}>
-                <div style={{ width: '15%', }}>
+            <div
+                style={{
+                    display: "flex",
+                    width: "100%",
+                    border: "1px solid red",
+                    marginBottom: "2rem",
+                }}
+            >
+                <div style={{ width: "15%" }}>
                     {Object.keys(content).map((keyName) => (
-                        <h5 className='apiKey' style={{ height: '30px', margin: '10px 0px', padding: '0 20px' }}> {keyName}</h5>
+                        <h5
+                            className="apiKey"
+                            style={{ height: "30px", margin: "10px 0px", padding: "0 20px" }}
+                        >
+                            {" "}
+                            {keyName}
+                        </h5>
                     ))}
                 </div>
-                <div style={{ width: '85%' }}>
+                <div style={{ width: "85%" }}>
                     {Object.keys(content).map((keyName) => {
-                        if (keyName == 'API') {
-                            return (<h5 className='apiKey' style={{ color: 'red', height: '30px', margin: '0' }}>
-                                {content[keyName]}</h5>)
-                        }
-                        else if (keyName == 'languages') {
+                        if (keyName == "API") {
+                            return (
+                                <h5
+                                    className="apiKey"
+                                    style={{ color: "red", height: "30px", margin: "0" }}
+                                >
+                                    {content[keyName]}
+                                </h5>
+                            );
+                        } else if (keyName == "languages") {
                             return (
                                 <div>
-                                    <Grid columns={5} >
-                                        <Grid.Row style={{ marginTop: 4, padding: 0 }} >
+                                    <Grid columns={5}>
+                                        <Grid.Row style={{ marginTop: 4, padding: 0 }}>
                                             {content[keyName].map((language, index) => (
                                                 <Grid.Column onClick={() => handleClick(language)}>
-                                                    <h5 style={{ padding: 10 }} className='language_list'> {language.language}</h5>
+                                                    <h5 style={{ padding: 10 }} className="language_list">
+                                                        {" "}
+                                                        {language.language}
+                                                    </h5>
                                                 </Grid.Column>
                                             ))}
                                         </Grid.Row>
                                     </Grid>
-                                    <Grid columns={1} >
-                                        <Grid.Row style={{ margin: 0, padding: 10 }} >
+                                    <Grid columns={1}>
+                                        <Grid.Row style={{ margin: 0, padding: 10 }}>
                                             {content[keyName].map((language, index) => (
-                                                <Grid.Column key={index}   >
-                                                    {clickedLanguage && clickedLanguage.language === language.language && (
-                                                        <code style={{}}>
-                                                            {language.code}
-                                                        </code>
-                                                    )}
+                                                <Grid.Column key={index}>
+                                                    {clickedLanguage &&
+                                                        clickedLanguage.language === language.language && (
+                                                            <div className="">
+                                                                <CopyToClipboard
+                                                                    text={language.code}
+                                                                    onCopy={() => setCopySuccess(true)}
+                                                                >
+                                                                    <button>
+                                                                        <Icon name="copy" />
+                                                                        {copySuccess && (
+                                                                            <span style={{ color: 'yellow' }}>
+                                                                                <br /><br />copied
+                                                                            </span>
+                                                                        )}
+                                                                    </button>
+                                                                </CopyToClipboard>
+                                                                <code style={{}}>
+                                                                    {language.code}
+
+                                                                </code>
+                                                            </div>
+                                                        )}
                                                 </Grid.Column>
                                             ))}
                                         </Grid.Row>
                                     </Grid>
+
                                 </div>
                             );
-                        }
-                        else {
-                            return (<h5 className='apiKey' style={{ background: '', height: '30px', margin: '10px 0px' }}>
-                                {content[keyName]}</h5>)
+                        } else {
+                            return (
+                                <h5
+                                    className="apiKey"
+                                    style={{ background: "", height: "30px", margin: "10px 0px" }}
+                                >
+                                    {content[keyName]}
+                                </h5>
+                            );
                         }
                     })}
                 </div>
-            </div >
+            </div>
         ));
-    }
+    };
 
     return (
         <>
-            <Container>
-                {mapComponents()}
-            </Container >
+            <Container>{mapComponents()}</Container>
         </>
-    )
-}
-export default Login
+    );
+};
+export default Login;
